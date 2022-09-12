83ae9e2d86c7752d17b401bfe98700661648316a
CrooKameron <85321458+CrooKameron@users.noreply.github.com>
2022-09-12 20:52:23 +0300

    v1.2

--- app.js
+++ app.js
@@ -1,4 +1,16 @@
 const player = new musicPlayer(musicList);
 
 
-player.getMusic();
+let music = player.getMusic();
+
+console.log(music.getName());
+
+player.next();
+
+music = player.getMusic();
+console.log(music.getName());
+
+player.previous();
+
+music = player.getMusic();
+console.log(music.getName());
