43a477137327ab0e04d33f3a0c3a413d2f62415b
CrooKameron <85321458+CrooKameron@users.noreply.github.com>
2022-09-13 20:24:13 +0300

    v1.3

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
+const container = document.querySelector(".container");
+const image = document.querySelector("#music-image");
+const title = document.querySelector("#music-details .title");
+const singer = document.querySelector("#music-details .singer");
+const prev = document.querySelector("#controls #prev");
+const paly = document.querySelector("#controls #paly");
+const next = document.querySelector("#controls #next");
+
 const player = new musicPlayer(musicList);
 
 
-let music = player.getMusic();
 
-console.log(music.getName());
 
+
+window.addEventListener("load", () => {
+    let music = player.getMusic();
+    displayMusic(music);
+})
  
+
+function displayMusic(music){
+    title.innerText = music.title;
+    singer.innerText = music.singer;
+    image.src = "img/" + music.img;
+    audio.src = "mp3/" + music.file;
+}
